137a6830e86c1eedec07149fec10def91875555e
nasirulla <rehmanansirulla1124@gmail.com>
2026-08-23 02:11:46 +0530

    Reel(loop) backend complete

---
+++ backend/routes/loop.route.js
@@ -0,0 +1,13 @@
+import express from "express"
+import isAuth from "../middleware/isAuth.js";
+import upload from "../middleware/multer.js";
+import { Comment, getAllLoops, Like, uploadLoop } from "../controllers/loop.controller.js";
+
+const loopRouter = express.Router();
+
+loopRouter.post("/upload-loop",isAuth ,upload.single("media"),uploadLoop);
+loopRouter.get("/get-loops",isAuth,getAllLoops);
+loopRouter.get("/like/:loopID",isAuth,Like);
+loopRouter.post("/comment/:loopID",isAuth,Comment);
+
+export default loopRouter;
